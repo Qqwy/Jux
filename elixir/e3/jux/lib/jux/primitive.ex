10b8b11f8d27964dcb7109ab5c603540850d131c
Qqwy/Wiebe-Marten <qqwy@gmx.com>
2017-03-16 23:32:35 +0100

    error messages, WIP renaming

--- elixir/e3/jux/lib/jux/primitive.ex
+++ elixir/e3/jux/lib/jux/primitive.ex
@@ -21,19 +21,33 @@ defmodule Jux.Primitive do
   end
 
   def define_new_word(state) do
-    [quotation | stack] = state.stack
-    dictionary = Jux.Dictionary.define_new_word(state.dictionary, quotation |> Jux.Quotation.implementation)
-    state
-    |> Map.put(:dictionary, dictionary)
-    |> Map.put(:stack, stack)
+    case state.stack do
+      [quotation | stack] ->
+        dictionary = Jux.Dictionary.define_new_word(state.dictionary, quotation |> Jux.Quotation.implementation)
+        state
+        |> Map.put(:dictionary, dictionary)
+        |> Map.put(:stack, stack)
+      _ ->
+        raise ArgumentError, "Cannot define new word because the stack does not have a quotation on top: #{inspect stack}"
   end
 
   def rename_last_word(state) do
-    # [name | stack] = state.stack
-    stack = state.stack
-    dictionary = Jux.Dictionary.rename_last_word(state.dictionary, "foo")
+    case state.stack do
+      [name | stack] when is_string(name) ->
+        dictionary = Jux.Dictionary.rename_last_word(state.dictionary, name)
+        state
+        |> Map.put(:dictionary, dictionary)
+        |> Map.put(:stack, stack)
+      _ ->
+        raise ArgumentError, "Cannot rename last word because the stack does not have a string on top: #{inspect stack}"
+    end
+  end
+
+  def heave_token_to_string(state) do
+    {word, unparsed_rest} = Jux.Parser.extract_token(state.unparsed_program)
+    stack = [word | stack]
     state
-    |> Map.put(:dictionary, dictionary)
+    |> Map.put(:unparsed_program, unparsed_rest)
     |> Map.put(:stack, stack)
   end
 
