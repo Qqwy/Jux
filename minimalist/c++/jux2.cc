a39c241826fa60d644dacd877ada2c48eb696237
Marten/Qqwy <w-m@wmcode.nl>
2021-01-25 23:19:41 +0100

    it works!

--- minimalist/c++/jux2.cc
+++ minimalist/c++/jux2.cc
@@ -16,6 +16,8 @@ word_t &latest = memory[3]; // address of beginning of newest dictionary entry
 word_t &here = memory[4]; // address of last instruction added to the dictionary
 
 
+#define debug if(false)
+// #define debug if(true)
 
 enum core_instruction {
   compileme = 0,
@@ -35,10 +37,10 @@ enum core_instruction {
 word_t const num_core_instructions = 13;
 
 void dump_memory() {
-  std::cout << "Memory dump:\n";
-  for(size_t index = 0; index < static_cast<size_t>(here); ++index) {
-    std::cout << index << ":  " << memory[index] << '\t' << static_cast<char>(memory[index]) << '\n';
-  }
+      std::cout << "Memory dump:\n";
+      for(size_t index = 0; index < static_cast<size_t>(here); ++index) {
+        std::cout << index << ":  " << memory[index] << '\t' << static_cast<char>(memory[index]) << '\n';
+      }
 }
 
 void push(word_t &stack_top, word_t value) {
@@ -81,6 +83,8 @@ std::string read_word() {
   std::cin >> str;
   if(str == "") {
     std::cout << "EOF encountered. Good bye!\n";
+
+    debug { dump_memory(); }
     exit(0);
   }
   return str;
@@ -89,7 +93,7 @@ std::string read_word() {
 // because we want to store individual characters in individual memory cells.
 // not very memory-efficient, but simple
 std::vector<word_t> wordname_to_wide_wordname(std::string wordname) {
-  std::cout << "converting `" << wordname << "` word_to wide format\n";
+  debug { std::cout << "converting `" << wordname << "` word_to wide format\n"; }
   std::vector<word_t> wide_wordname;
   std::copy(wordname.begin(), wordname.end(), std::back_inserter(wide_wordname));
   return wide_wordname;
@@ -97,26 +101,28 @@ std::vector<word_t> wordname_to_wide_wordname(std::string wordname) {
 
 word_t lookup_in_dictionary(std::vector<word_t> wide_wordname) {
   word_t size = wide_wordname.size();
-  word_t dictionary_entry = memory[latest];
+  word_t dictionary_entry = latest;
   while (true) {
-    std::cout << "Comparing against dictionary entry " << dictionary_entry << '\n';
+    debug { std::cout << "Comparing against dictionary entry " << dictionary_entry << '\n'; }
 
     if(dictionary_entry == 0) {
       return 0;
     }
-    word_t prev = memory[dictionary_entry];
     word_t entry_name_length = memory[dictionary_entry + 1];
     word_t *entry_name_ptr = &memory[dictionary_entry + 2];
 
     if(size == entry_name_length && memcmp(wide_wordname.data(), entry_name_ptr, size * sizeof(word_t)) == 0) {
-      for(word_t index = 0; index < size; ++index) {
-        std::cout << wide_wordname[index]  << " vs. " << entry_name_ptr[index] << '\n';
+      debug {
+        for(word_t index = 0; index < size; ++index) {
+          std::cout << wide_wordname[index]  << " vs. " << entry_name_ptr[index] << '\n';
+        }
+       std::cout << "success\n";
       }
-      std::cout << "success\n";
+
       return dictionary_entry;
     }
 
-    dictionary_entry = prev;
+    dictionary_entry = memory[dictionary_entry];
   }
 }
 
@@ -134,7 +140,7 @@ word_t dictionary_entry_to_codeword_location(word_t dictionary_entry) {
 
 void run_instruction(word_t instruction_address);
 void run_compile_word() {
-  std::cout << "compile_word\n";
+  debug { std::cout << "compile_word\n"; }
 
     std::string wordname = read_word();
     int dictionary_address = lookup_in_dictionary(wordname);
@@ -144,20 +150,21 @@ void run_compile_word() {
     } else {
       try {
         int val = std::stoi(wordname);
+        // push_dict(pushint);
         push_dict(dictionary_entry_to_codeword_location(lookup_in_dictionary("pushint")) + 1);
         push_dict(val);
       } catch (...) {
-        dump_memory();
+        debug { dump_memory(); }
 
         std::cerr << "error: `" << wordname << "` cannot be found in the dictionary but is also not a number literal. Exiting.\n";
         exit(1);
       }
     }
-    std::cout << "end of compile_word\n";
+    debug { std::cout << "end of compile_word\n"; }
 }
 
 void run_define() {
-  std::cout << "define\n";
+  debug { std::cout << "define\n"; }
   std::vector<word_t> wide_wordname = wordname_to_wide_wordname(read_word());
   push_dict(latest); // link to previous dictionary entry
   latest = here - 1; // update most recent entry pointer to point to current dictionary entry (which starts with the link to the previous entry, hence the - 1)
@@ -166,41 +173,48 @@ void run_define() {
     push_dict(character);
   }
   push_dict(compileme);
-  std::cout << "end of define\n";
+  push_dict(runme); // This is what we compile a pointer to
+  debug { std::cout << "end of define\n"; }
 }
 
 void run_immediate() {
-  std::cout << "immediate\n";
-  pop_dict();
-  push_dict(runme);
+  debug { std::cout << "immediate\n"; }
+  pop_dict(); // remove the 'runme'
+  pop_dict(); // remove the 'compileme'
+  push_dict(runme); // only have the 'runme'.
 }
 
 void run_instruction(word_t instruction_address) {
   core_instruction instruction = static_cast<core_instruction>(memory[instruction_address]);
-  std::cout << "instruction: " << instruction << "\t";
+  if(instruction >= num_core_instructions) {
+    std::cerr << "Error. Encountered unrunnable instruction " << instruction << "\n";
+    debug { dump_memory(); }
+    exit(1);
+  }
+  debug { std::cout << "instruction: " << instruction << "\t"; }
   switch(instruction) {
   case compileme: // TODO doubt
     {
-      std::cout << "compileme\n";
+      debug { std::cout << "compileme\n"; }
       push_dict(instruction_address + 1);
     }
     break;
   case runme: // TODO doubt
     {
-      std::cout << "runme\n";
+      debug { std::cout << "runme\n"; }
       push_call(pc); // store 'next instruction to be run' on callstack
       pc = instruction_address + 1; // and replace it for now with the instruction following this one.
     }
     break;
   case pushint:
     {
-      // the pc points not to an instruction but to the numeric value to push,
-      // so push it and skip it.
-      std::cout << "pushint\n";
+      debug { std::cout << "pushint\n"; }
       int val = memory[pc];
+      debug { std::cout << "Pushing value: " << val << "\n"; }
       ++pc;
       push_value(val);
     }
+    break;
   case compile_word:
     run_compile_word();
     break;
@@ -212,13 +226,13 @@ void run_instruction(word_t instruction_address) {
     break;
   case returntocaller:
     {
-      std::cout << "return\n";
+      debug { std::cout << "return\n"; }
       pc = pop_call();
     }
     break;
   case subtract:
     {
-      std::cout << "subtract\n";
+      debug { std::cout << "subtract\n"; }
       int rhs = pop_value();
       int lhs = pop_value();
       int result = lhs - rhs;
@@ -227,7 +241,7 @@ void run_instruction(word_t instruction_address) {
     break;
   case negative:
     {
-      std::cout << "negative\n";
+      debug { std::cout << "negative\n"; }
       int val = pop_value();
       int result = val < 0;
       push_value(result);
@@ -235,7 +249,7 @@ void run_instruction(word_t instruction_address) {
     break;
   case fetch:
     {
-      std::cout << "fetch\n";
+      debug { std::cout << "fetch\n"; }
       int memory_ptr = pop_value();
       int val = memory[memory_ptr];
       push_value(val);
@@ -243,7 +257,7 @@ void run_instruction(word_t instruction_address) {
     break;
   case store:
     {
-      std::cout << "store\n";
+      debug { std::cout << "store\n"; }
       int memory_ptr = pop_value();
       int val = pop_value();
       memory[memory_ptr] = val;
@@ -251,14 +265,14 @@ void run_instruction(word_t instruction_address) {
     break;
   case getcharacter:
     {
-      std::cout << "getchar\n";
+      debug { std::cout << "getchar\n"; }
       int val = getchar();
       push_value(val);
     }
     break;
   case putcharacter:
     {
-      std::cout << "putchar\n";
+      debug { std::cout << "putchar\n"; }
       int val = pop_value();
       putchar(val);
     }
@@ -267,41 +281,35 @@ void run_instruction(word_t instruction_address) {
 }
 
 void run_instructions() {
-  size_t count = 0;
+  // size_t count = 0;
   while(true) {
-    std::cout << count << ": ";
+    // debug { std::cout << count << ": "; }
 
-    // core_instruction instruction_address = static_cast<core_instruction>(memory[pc]);
-    word_t instruction_address(memory[pc]);
+    word_t instruction_address = memory[pc];
     ++pc;
 
-    std::cout << "\t pc: " << pc << "\t m[pc-1]: " << instruction_address << "\t";
+    debug { std::cout << "\t pc: " << pc << "\t m[pc-1]: " << instruction_address << "\t"; }
 
-    // during `run_instruction`, `pc` will always point to the address of the next codeword to be run.
     run_instruction(instruction_address);
 
-    ++count;
-    if(count > 100) {
-      break;
-    }
+    // ++count;
+    // if(count > 200) {
+    //   break;
+    // }
   }
 }
 
 void initialize_dictionary() {
   for(word_t instruction = 0; instruction < num_core_instructions; ++instruction) {
     run_define();
+    pop_dict();
+
+    // Ensures that `define` and `immediate` run immediately
     if(instruction == define || instruction == immediate){
-      // run_immediate();
       pop_dict();
     }
     push_dict(instruction);
-    push_dict(returntocaller);
   }
-  // for(word_t instruction = 5; instruction < num_core_instructions; ++instruction) {
-  //   run_define();
-  //   push_dict(instruction);
-  //   push_dict(returntocaller);
-  // }
 }
 
 void initialize_inner_interpreter() {
@@ -322,7 +330,7 @@ int main() {
 
   initialize_dictionary();
   initialize_inner_interpreter();
-  dump_memory();
+  debug { dump_memory(); }
 
   run_instructions();
   dump_memory();
