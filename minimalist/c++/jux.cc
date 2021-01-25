57b5c1cd661cc3522d5db367215c4fe17e9d06a6
Marten/Qqwy <w-m@wmcode.nl>
2021-01-25 01:47:34 +0100

    WIP

--- minimalist/c++/jux.cc
+++ minimalist/c++/jux.cc
@@ -1,6 +1,12 @@
-#include <iostream>
+#include <iostream> // std::cin, std::cout
+#include <string>  // std::string
+#include <vector> // vector
+#include <cstring> // memcmp
 
-int memory[20000] = {0, 0, 0, 0, 0, 0, 64};
+#include <array> // array
+
+// int memory[20000] = {0, 0, 0, 0, 0, 0, 64};
+std::array<int, 20000> memory;
 int *pc = &memory[0];
 int *t = &memory[1];
 int *r = &memory[2];
@@ -23,6 +29,7 @@ enum core_instruction {
   immediate,
   returntocaller
 };
+int const num_core_instructions = 13;
 
 void push(int * stack_top, int value) {
   memory[*stack_top] = value;
@@ -54,6 +61,59 @@ int pop_call() {
 void push_dict(int value) {
   push(h, value);
 }
+int pop_dict() {
+  return pop(h);
+}
+
+std::string read_word() {
+  std::string str;
+  if(!std::cin) {
+    std::cout << "EOF encountered. Good bye!\n";
+    exit(0);
+  }
+  std::cin >> str;
+  return str;
+}
+
+// because we want to store individual characters in individual memory cells.
+// not very memory-efficient, but simple
+std::vector<int> wordname_to_wide_wordname(std::string wordname) {
+  std::cout << "converting `" << wordname << "` into wide format\n";
+  std::vector<int> wide_wordname;
+  std::copy(wordname.begin(), wordname.end(), std::back_inserter(wide_wordname));
+  return wide_wordname;
+}
+
+int lookup_in_dictionary(std::vector<int> wide_wordname) {
+  const int *wide_wordname_ptr = wide_wordname.data();
+  int length = wide_wordname.size();
+
+  int dictionary_entry = memory[*here];
+  while (true) {
+    std::cout << "Comparing against dictionary entry " << dictionary_entry << '\n';
+
+    if(dictionary_entry == 0) {
+      return 0;
+    }
+    int prev = memory[dictionary_entry];
+    int entry_name_length = memory[dictionary_entry + 1];
+    int *entry_name_ptr = &memory[dictionary_entry + 2];
+
+    if(length == entry_name_length && memcmp(wide_wordname_ptr, entry_name_ptr, length) == 0) {
+      std::cout << "success\n";
+      return dictionary_entry;
+    }
+
+    dictionary_entry = prev;
+  }
+}
+
+int dictionary_entry_to_codeword_location(int dictionary_entry) {
+  int entry_name_length = memory[dictionary_entry + 1];
+  int codeword_location = dictionary_entry + 2 + entry_name_length;
+  return codeword_location;
+}
+
 
 // lodsl-like implementation
 void run_instruction(core_instruction instruction) {
@@ -66,7 +126,7 @@ void run_instruction(core_instruction instruction) {
   case runme:
     {
       push_call(*pc);
-      pc = &memory[*pc];
+      pc = &memory[*pc] - 1;
     }
     break;
   case pushint:
@@ -77,8 +137,39 @@ void run_instruction(core_instruction instruction) {
     }
     break;
   case compile_word:
+    {
+      std::cout << "Lookup in compile_word\n";
+      std::string wordname = read_word();
+      int dictionary_address = lookup_in_dictionary(wordname_to_wide_wordname(wordname));
+      if(dictionary_address != 0) {
+        int codeword_location = dictionary_entry_to_codeword_location(dictionary_address);
+        // push_dict(codeword_location);
+        // *pc = codeword_location - 1;
+        run_instruction(static_cast<core_instruction>(memory[codeword_location]));
+      } else {
+        try {
+          int val = std::stoi(wordname);
+          push_dict(pushint);
+          push_dict(val);
+        } catch (...) {
+          std::cerr << "error: `" << wordname << "` cannot be found in the dictionary but is also not a number literal. Exiting.\n";
+          exit(1);
+        }
+      }
+    }
     break;
   case define:
+    {
+      std::cout << "Lookup in define\n";
+      std::vector<int> wide_wordname = wordname_to_wide_wordname(read_word());
+      push_dict(*here); // link to previous dictionary entry
+      *here = *h - 1; // update most recent entry pointer to point to current dictionary entry (which starts with the link to the previous entry, hence the - 1)
+      push_dict(wide_wordname.size());
+      for(auto character : wide_wordname) {
+        push_dict(character);
+      }
+      push_dict(compileme);
+    }
     break;
   case subtract:
     {
@@ -122,15 +213,73 @@ void run_instruction(core_instruction instruction) {
     }
     break;
   case immediate:
+    {
+      pop_dict();
+      push_dict(runme);
+    }
     break;
   case returntocaller:
-    *pc = pop_call();
+    {
+      *pc = pop_call();
+    }
     break;
   }
+}
 
-  ++pc;
+void define_core_instruction(core_instruction instruction) {
+  run_instruction(define);
+
+  // put the instruction in the codeword section of the dictionary entry
+  pop_dict();
+  push_dict(instruction);
 }
 
 int main() {
-  
+  // initialize machine
+  // initialize stack pointers
+  *r = 8; // start of call stack is after special-value region. It is 1024 words large. Stackoverflow is not detected.
+  *h = *r + 128; // start of dictionary is after call stack region.
+  *t = 10000; // start of value stack is halfway through memory.
+
+  // set up core instructions in dictionary
+  for(size_t index = 0; index < num_core_instructions; ++ index) {
+    define_core_instruction(static_cast<core_instruction>(index));
+  }
+
+  // define first version of internal interpreter:
+  // compile a word and then call itself (i.e. forever repeating this procedure)
+  run_instruction(define);
+  run_instruction(immediate);
+  int myself = *h - 1;
+  push_dict(dictionary_entry_to_codeword_location(lookup_in_dictionary(wordname_to_wide_wordname("compile_word"))));
+  push_dict(myself);
+
+  *pc = myself;
+
+  std::cout << "Memory dump:\n";
+  for(size_t index = 0; index < 512; ++index) {
+    std::cout << index << ":  " << memory[index] << '\t' << static_cast<char>(memory[index]) << '\n';
+  }
+
+
+  size_t no_instruction = 0;
+      // start program execution proper
+  while(true) {
+    ++no_instruction;
+    if(no_instruction > 100) {
+      break;
+    }
+
+    std::cout << no_instruction << " pc: " << *pc << '\n';
+    std::cout << no_instruction << " memory[pc]: "<< memory[*pc] << '\n';
+    core_instruction instruction = static_cast<core_instruction>(memory[*pc]);
+    run_instruction(instruction);
+    ++pc;
+  }
+
+  std::cout << "Memory dump:\n";
+  for(size_t index = 0; index < 512; ++index) {
+    std::cout << index << ":  " << memory[index] << '\t' << static_cast<char>(memory[index]) << '\n';
+  }
+
 }
